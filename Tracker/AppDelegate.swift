5376c70aa960de41806e4e13a33fff86c64e0b67
Pavel Bobkov <pavel.bbkv1@gmail.com>
2024-09-26 00:23:32 +0300

    update AppDelegate

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -9,9 +9,13 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        window = UIWindow()
+        window?.rootViewController = TabBarController()
+        window?.makeKeyAndVisible()
         return true
     }
 
